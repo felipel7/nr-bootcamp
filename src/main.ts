2b9eacca462abcc2e04c8c3f8a19c4ce61d3aba8
felipel7 <felipelseven@gmail.com>
2024-09-29 21:29:12 -0300

    feat: exercise 01 implementation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,53 @@
+import { findVowels } from './exercises/exercise-01';
 import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <section>
-    <h2>Orion BootCamp Exercises</h2>
-  </section>
-`;
+/**
+ *
+ * a) Dar um exemplo de uso com uma palavra recebida via parâmetro da função.
+ *
+ */
+const phrase = 'Olá mundo!';
+const vowelsCount = findVowels(phrase);
+console.log(createAnswerMessage(phrase, vowelsCount));
+
+/**
+ *
+ * b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
+ *
+ */
+document
+  .getElementById('text-form')
+  ?.addEventListener('submit', ($event: SubmitEvent) => {
+    $event.preventDefault();
+
+    const $form = new FormData($event.target as HTMLFormElement);
+    const inputValue = String($form.get('text-input'));
+    if (!inputValue) return;
+
+    const vowelsCount = findVowels(inputValue);
+    const message = createAnswerMessage(inputValue, vowelsCount);
+
+    appendAnswerMessage(message);
+  });
+
+function createAnswerMessage(phrase: string, vowelsCount: number) {
+  return `A frase: "${phrase}" possui ${vowelsCount} vogais.`;
+}
+
+// Adiciona a resposta no DOM
+function appendAnswerMessage(message: string) {
+  let $container = document.getElementById('first-exercise-answer');
+
+  if (!$container) {
+    $container = document.createElement('p');
+    $container.id = 'first-exercise-answer';
+    document.getElementById('first-exercise')?.appendChild($container);
+  }
+
+  $container.innerText = message;
+}
+
+// Retira a resposta do DOM quando o usuário limpa o form
+document.getElementById('text-form')?.addEventListener('reset', () => {
+  document.getElementById('first-exercise-answer')?.remove();
+});
